0090a3cae4fe3f5587af858e6e0f6c16cba5fb8b
juju1023 <74400736+juju1023@users.noreply.github.com>
2020-11-13 16:07:08 +0800

    Add files via upload

---
+++ myBlocks/blockly/blocks.js
@@ -0,0 +1,10 @@
+Blockly.Blocks['say_hello'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("say hello");
+    this.setOutput(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ myBlocks/blockly/javascript.js
@@ -0,0 +1,6 @@
+Blockly.JavaScript['say_hello'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '("say hello...")';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
